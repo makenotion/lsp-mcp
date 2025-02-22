a0d4536c8045c493af06dc1deb78164af4e79889
Jon Radchenko <jonrad@gmail.com>
2025-02-22 15:54:21 -0500

    async

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from multilspy import SyncLanguageServer
+from multilspy import LanguageServer
 from multilspy.multilspy_config import MultilspyConfig
 from multilspy.multilspy_logger import MultilspyLogger
 import os
 import asyncio
 
-def main():
+async def main():
     cwd = os.getcwd()
     config = MultilspyConfig.from_dict({"code_language": "python"})
     logger = MultilspyLogger()
-    lsp = SyncLanguageServer.create(config, logger, cwd)
+    lsp = LanguageServer.create(config, logger, cwd)
     print("LSP starting...")
-    with lsp.start_server():
+    async with lsp.start_server():
         print("LSP started")
 
         try:
-            result = lsp.request_definition("main.py", 6, 5)
+            result = await lsp.request_definition("main.py", 7, 5)
             print(result)
         except Exception as e:
             print(f"Error getting definition: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
