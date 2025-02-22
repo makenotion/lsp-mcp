94e1f379c698423bd7ac085f7d78157e1acaef4d
Jon Radchenko <jonrad@gmail.com>
2025-02-22 18:51:36 -0500

    connected mcp

--- README.md
+++ README.md
@@ -12,7 +12,13 @@
 ### Running
 ### Testing
 ### Debugging
-## Decisions
+### Helpful Scripts
+Putting these here until I make a decision on how to handle them (uv or task or something else)
+```
+# Helpful MCP Client without actually using an LLM (Call your tools directly)
+npx @wong2/mcp-cli --config config.json
+```
+### Decisions
 * Using python - I want to leverage a client library that makes the startup of this simple. A lot of LSPs are created in node, but the mature client libraries seem to be dependent on vscode. I like the look of [multilspy](https://github.com/microsoft/multilspy), so we'll start with python. It helps that I already created a python MCP, so at least I'll have a leg up there
 * [uv](https://docs.astral.sh/uv/)  for package management and such - I've been seeing this used more frequently lately and this is an excuse to learn it. Switching package managers in the future is annoying but doable. I may have to revisit this decision once implementing CI/CD. Maybe I can use this instead of a dependency on [taskfile](https://taskfile.dev/) as well? TBD
 * Async when possible - It's 2025
--- main.py
+++ main.py
@@ -1,25 +1,44 @@
+from typing import Callable
 from multilspy import LanguageServer
 from multilspy.multilspy_config import MultilspyConfig
 from multilspy.multilspy_logger import MultilspyLogger
+from mcp.server.fastmcp import FastMCP
+from mcp.types import (
+    Tool as MCPTool,
+)
+import inspect
 import os
 import asyncio
+
 cwd = os.getcwd()
 
 def get_relative_path(file_path: str) -> str:
     return os.path.join(cwd, file_path)
 
+def build_mcp(lsp: LanguageServer) -> FastMCP:
+    mcp = FastMCP("lsp")
+    mcp.add_tool(lsp.request_document_symbols)
+    return mcp
+
+def mcp_tool_from_function(fn: Callable) -> MCPTool:
+    return MCPTool(
+        fn=fn,
+        name=fn.__name__,
+        description=inspect.getdoc(fn),
+    )
+
 async def main():
     config = MultilspyConfig.from_dict({"code_language": "python"})
     logger = MultilspyLogger()
     lsp = LanguageServer.create(config, logger, cwd)
+
+    mcp = build_mcp(lsp)
+
     print("LSP starting...")
     async with lsp.start_server():
         print("LSP started")
 
-        path = get_relative_path("main.py")
-        with lsp.open_file(get_relative_path(path)):
-            result = lsp.get_open_file_text(path)
-            print(result)
+        await mcp.run_stdio_async()
 
 
 if __name__ == "__main__":
