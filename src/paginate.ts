dd928d899295b0832d5928753190437ded8beb2c
eagarwal-notion <eagarwal@makenotion.com>
2025-06-20 15:58:58 -0700

    feat: Add Pagination support (#4)

---
+++ src/paginate.ts
@@ -0,0 +1,15 @@
+const RESULTS_PER_PAGE = 10;
+export function paginateResponse(response: string, page: number): string {
+	let pages = 1;
+	if (Array.isArray(response)) {
+		pages = Math.ceil(response.length / RESULTS_PER_PAGE);
+		response = response.slice(
+			page * RESULTS_PER_PAGE,
+			(page + 1) * RESULTS_PER_PAGE,
+		);
+	}
+	return JSON.stringify({
+		result: response,
+		num_pages: pages,
+	});
+}
