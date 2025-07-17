48f8b2e210580a17865c74b414c9e05efa5b81d1
eagarwal-notion <eagarwal@makenotion.com>
2025-07-17 16:18:37 -0700

    test: Switch to Vitest (#29)
    Switches the testing framework to vitest, since it's faster and supports typescript better.
    Reformats some test files

--- src/FileWatcher.test.ts
+++ src/FileWatcher.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, afterEach, test, describe, expect, jest } from "@jest/globals";
+import { beforeEach, afterEach, test, describe, expect, vi } from "vitest";
 import { FileWatcher } from "./FileWatcher";
 import { consoleLogger } from "./logger";
 import path, { join } from "node:path";
@@ -6,47 +6,53 @@ import { mkdir, rm, writeFile } from "node:fs/promises";
 import { pathToFileUri } from "./lsp-methods";
 
 describe("File Watcher tests", () => {
-  let watcher: FileWatcher
-  const onAdd = jest.fn<(_: string, __: string) => Promise<void>>()
-  const onChange = jest.fn<(_: string, __: string) => Promise<void>>()
-  const onRemove = jest.fn<(_: string) => Promise<void>>()
-  const testDir = path.resolve("__test__")
-  const target = join(testDir, "test.txt")
-  const targetUri = pathToFileUri(target)
-  beforeEach(async () => {
-    try {
-      await rm(testDir, { recursive: true })
-    } catch (e: unknown) {
-      // TODO: fix this
-    }
-    await mkdir(testDir, {})
-    await new Promise(r => setTimeout(r, 300))
-    watcher = new FileWatcher([".txt"], testDir, consoleLogger, onChange, onRemove, onAdd)
-    await watcher.start()
-
-  })
-  afterEach(async () => {
-    await watcher.dispose()
-    await rm(testDir, { recursive: true })
-    await new Promise(r => setTimeout(r, 300))
-  })
-  test("Add File", async () => {
-    await writeFile(target, "test_Data")
-    await new Promise(r => setTimeout(r, 300))
-    expect(onAdd).toHaveBeenCalledWith(targetUri, "test_Data")
-  })
-  test("Update File", async () => {
-    await writeFile(target, "test_Data")
-    await new Promise(r => setTimeout(r, 300))
-    await writeFile(target, "new_Data")
-    await new Promise(r => setTimeout(r, 300))
-    expect(onChange).toHaveBeenCalledWith(targetUri, "new_Data")
-  })
-  test("Remove File", async () => {
-    await writeFile(target, "test_Data")
-    await new Promise(r => setTimeout(r, 300))
-    await rm(target)
-    await new Promise(r => setTimeout(r, 300))
-    expect(onRemove).toHaveBeenCalledWith(targetUri)
-  })
-})
+	let watcher: FileWatcher;
+	const onAdd = vi.fn<(_: string, __: string) => Promise<void>>();
+	const onChange = vi.fn<(_: string, __: string) => Promise<void>>();
+	const onRemove = vi.fn<(_: string) => Promise<void>>();
+	const testDir = path.resolve("__test__");
+	const target = join(testDir, "test.txt");
+	const targetUri = pathToFileUri(target);
+	beforeEach(async () => {
+		try {
+			await rm(testDir, { recursive: true });
+		} catch (e: unknown) {
+			// TODO: fix this
+		}
+		await mkdir(testDir, {});
+		await new Promise((r) => setTimeout(r, 300));
+		watcher = new FileWatcher(
+			[".txt"],
+			testDir,
+			consoleLogger,
+			onChange,
+			onRemove,
+			onAdd,
+		);
+		await watcher.start();
+	});
+	afterEach(async () => {
+		await watcher.dispose();
+		await rm(testDir, { recursive: true });
+		await new Promise((r) => setTimeout(r, 300));
+	});
+	test("Add File", async () => {
+		await writeFile(target, "test_Data");
+		await new Promise((r) => setTimeout(r, 300));
+		expect(onAdd).toHaveBeenCalledWith(targetUri, "test_Data");
+	});
+	test("Update File", async () => {
+		await writeFile(target, "test_Data");
+		await new Promise((r) => setTimeout(r, 300));
+		await writeFile(target, "new_Data");
+		await new Promise((r) => setTimeout(r, 300));
+		expect(onChange).toHaveBeenCalledWith(targetUri, "new_Data");
+	});
+	test("Remove File", async () => {
+		await writeFile(target, "test_Data");
+		await new Promise((r) => setTimeout(r, 300));
+		await rm(target);
+		await new Promise((r) => setTimeout(r, 300));
+		expect(onRemove).toHaveBeenCalledWith(targetUri);
+	});
+});
