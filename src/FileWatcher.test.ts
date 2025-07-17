ff1a0576f15253b940e56b5cdd0e5bfae402bf99
Ellen Agarwal <eagarwal@makenotion.com>
2025-07-17 15:14:30 -0700

    misc: Add type hints and reformat

--- src/FileWatcher.test.ts
+++ src/FileWatcher.test.ts
@@ -1,26 +1,26 @@
-import { beforeEach, afterEach, test, describe, expect, vi } from "vitest";
-import { FileWatcher } from "./FileWatcher";
-import { consoleLogger } from "./logger";
-import path, { join } from "node:path";
-import { mkdir, rm, writeFile } from "node:fs/promises";
-import { pathToFileUri } from "./lsp-methods";
+import { mkdir, rm, writeFile } from "node:fs/promises"
+import path, { join } from "node:path"
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest"
+import { FileWatcher } from "./FileWatcher"
+import { consoleLogger } from "./logger"
+import { pathToFileUri } from "./lsp-methods"
 
 describe("File Watcher tests", () => {
-	let watcher: FileWatcher;
-	const onAdd = vi.fn<(_: string, __: string) => Promise<void>>();
-	const onChange = vi.fn<(_: string, __: string) => Promise<void>>();
-	const onRemove = vi.fn<(_: string) => Promise<void>>();
-	const testDir = path.resolve("__test__");
-	const target = join(testDir, "test.txt");
-	const targetUri = pathToFileUri(target);
+	let watcher: FileWatcher
+	const onAdd = vi.fn<(_: string, __: string) => Promise<void>>()
+	const onChange = vi.fn<(_: string, __: string) => Promise<void>>()
+	const onRemove = vi.fn<(_: string) => Promise<void>>()
+	const testDir = path.resolve("__test__")
+	const target = join(testDir, "test.txt")
+	const targetUri = pathToFileUri(target)
 	beforeEach(async () => {
 		try {
-			await rm(testDir, { recursive: true });
-		} catch (e: unknown) {
+			await rm(testDir, { recursive: true })
+		} catch (_: unknown) {
 			// TODO: fix this
 		}
-		await mkdir(testDir, {});
-		await new Promise((r) => setTimeout(r, 300));
+		await mkdir(testDir, {})
+		await new Promise(r => setTimeout(r, 300))
 		watcher = new FileWatcher(
 			[".txt"],
 			testDir,
@@ -28,31 +28,31 @@ describe("File Watcher tests", () => {
 			onChange,
 			onRemove,
 			onAdd,
-		);
-		await watcher.start();
-	});
+		)
+		await watcher.start()
+	})
 	afterEach(async () => {
-		await watcher.dispose();
-		await rm(testDir, { recursive: true });
-		await new Promise((r) => setTimeout(r, 300));
-	});
+		await watcher.dispose()
+		await rm(testDir, { recursive: true })
+		await new Promise(r => setTimeout(r, 300))
+	})
 	test("Add File", async () => {
-		await writeFile(target, "test_Data");
-		await new Promise((r) => setTimeout(r, 300));
-		expect(onAdd).toHaveBeenCalledWith(targetUri, "test_Data");
-	});
+		await writeFile(target, "test_Data")
+		await new Promise(r => setTimeout(r, 300))
+		expect(onAdd).toHaveBeenCalledWith(targetUri, "test_Data")
+	})
 	test("Update File", async () => {
-		await writeFile(target, "test_Data");
-		await new Promise((r) => setTimeout(r, 300));
-		await writeFile(target, "new_Data");
-		await new Promise((r) => setTimeout(r, 300));
-		expect(onChange).toHaveBeenCalledWith(targetUri, "new_Data");
-	});
+		await writeFile(target, "test_Data")
+		await new Promise(r => setTimeout(r, 300))
+		await writeFile(target, "new_Data")
+		await new Promise(r => setTimeout(r, 300))
+		expect(onChange).toHaveBeenCalledWith(targetUri, "new_Data")
+	})
 	test("Remove File", async () => {
-		await writeFile(target, "test_Data");
-		await new Promise((r) => setTimeout(r, 300));
-		await rm(target);
-		await new Promise((r) => setTimeout(r, 300));
-		expect(onRemove).toHaveBeenCalledWith(targetUri);
-	});
-});
+		await writeFile(target, "test_Data")
+		await new Promise(r => setTimeout(r, 300))
+		await rm(target)
+		await new Promise(r => setTimeout(r, 300))
+		expect(onRemove).toHaveBeenCalledWith(targetUri)
+	})
+})
--- src/progress.ts
+++ src/progress.ts
@@ -1,39 +1,45 @@
-import { WorkDoneProgressBegin, WorkDoneProgressEnd, WorkDoneProgressReport } from "vscode-languageserver-protocol";
-import * as rpc from "vscode-jsonrpc";
-import { ProgressNotification } from "@modelcontextprotocol/sdk/types.js";
+import type { ProgressNotification } from "@modelcontextprotocol/sdk/types.js"
+import type * as rpc from "vscode-jsonrpc"
+import type {
+	WorkDoneProgressBegin,
+	WorkDoneProgressEnd,
+	WorkDoneProgressReport,
+} from "vscode-languageserver-protocol"
 
-export function convertLspToMcp(message: WorkDoneProgressBegin | WorkDoneProgressReport | WorkDoneProgressEnd, token: rpc.ProgressToken): ProgressNotification {
-
-  switch (message.kind) {
-    case "begin":
-      return {
-        method: "notifications/progress",
-        params: {
-          progress: message.percentage ?? 0,
-          progressToken: token,
-          total: 100,
-          message: message.message ?? message.title
-        }
-      }
-    case "report":
-      return {
-        method: "notifications/progress",
-        params: {
-          progress: message.percentage ?? 0,
-          progressToken: token,
-          total: 100,
-          message: message.message ?? undefined
-        }
-      }
-    case "end":
-      return {
-        method: "notifications/progress",
-        params: {
-          progress: 100,
-          progressToken: token,
-          total: 100,
-          message: message.message ?? undefined
-        }
-      }
-  }
+export function convertLspToMcp(
+	message: WorkDoneProgressBegin | WorkDoneProgressReport | WorkDoneProgressEnd,
+	token: rpc.ProgressToken,
+): ProgressNotification {
+	switch (message.kind) {
+		case "begin":
+			return {
+				method: "notifications/progress",
+				params: {
+					progress: message.percentage ?? 0,
+					progressToken: token,
+					total: 100,
+					message: message.message ?? message.title,
+				},
+			}
+		case "report":
+			return {
+				method: "notifications/progress",
+				params: {
+					progress: message.percentage ?? 0,
+					progressToken: token,
+					total: 100,
+					message: message.message ?? undefined,
+				},
+			}
+		case "end":
+			return {
+				method: "notifications/progress",
+				params: {
+					progress: 100,
+					progressToken: token,
+					total: 100,
+					message: message.message ?? undefined,
+				},
+			}
+	}
 }
