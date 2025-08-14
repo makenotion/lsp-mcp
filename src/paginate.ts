127f86988e58f1db628c00591987f58711681356
Ellen Agarwal <eagarwal@makenotion.com>
2025-08-14 10:09:30 -0700

    feat: Add pagination to diagnostics endpoint

--- src/paginate.ts
+++ src/paginate.ts
@@ -1,11 +1,17 @@
+import type { JSONValue } from "./utils"
+
 const RESULTS_PER_PAGE = 10
-export function paginateResponse(response: string, page: number): string {
+export function paginateResponse(
+	response: string | JSONValue[],
+	page: number,
+	resultsPerPage: number = RESULTS_PER_PAGE,
+): string {
 	let pages = 1
 	if (Array.isArray(response)) {
-		pages = Math.ceil(response.length / RESULTS_PER_PAGE)
+		pages = Math.ceil(response.length / resultsPerPage)
 		response = response.slice(
-			page * RESULTS_PER_PAGE,
-			(page + 1) * RESULTS_PER_PAGE,
+			page * resultsPerPage,
+			(page + 1) * resultsPerPage,
 		)
 	}
 	return JSON.stringify({
